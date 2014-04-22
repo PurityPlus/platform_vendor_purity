c21002432ddd2dd72d2aff7aa13e99c8258fcc2f
AndroidRul3z <ferrettidario@gmail.com>
2014-04-22 14:21:16 +0200

    Cleanup

--- config/common.mk
+++ config/common.mk
@@ -17,17 +17,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     keyguard.no_require_sim=true \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
     ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
-    ro.com.google.clientidbase=android-google \
     ro.com.android.wifi-watchlist=GoogleGuest \
-    ro.com.google.locationfeatures=1 \
     ro.setupwizard.enterprise_mode=1 \
-    ro.com.android.dateformat=MM-dd-yyyy \
-    windowsmgr.max_events_per_sec=240 \
-    ro.kernel.android.checkjni=0
-
-# Disable GC freed lines
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.debug.alloc=0
+    ro.com.android.dateformat=MM-dd-yyyy
 
 # Include overlays
 PRODUCT_PACKAGE_OVERLAYS += vendor/purity/overlay/common
