60548649cbbeba2bea5005f02952b084437d535f
brjshshrma <brajesh.sharma87@gmail.com>
2014-07-14 01:15:49 +0530

    Xposed framework OTA survival

--- prebuilt/bin/backuptool.sh
+++ prebuilt/bin/backuptool.sh
@@ -19,6 +19,25 @@ restore_addon_d() {
   rm -rf /tmp/addon.d/
 }
 
+# Backup Xposed Framework (bin/app_process)
+xposed_backup()
+{
+	if ( grep -ciE ".*with Xposed support \\(version (.+)\\).*" /system/bin/app_process )
+		then
+			cp /system/bin/app_process /tmp/backupdir/
+	fi
+}
+
+# Restore Xposed Framework (bin/app_process)
+xposed_restore()
+{
+	if [ -f /tmp/backupdir/app_process ]
+		then
+			mv /system/bin/app_process /system/bin/app_process.orig
+			cp /tmp/backupdir/app_process /system/bin/
+	fi
+}
+
 # Execute /system/addon.d/*.sh scripts with $1 parameter
 run_stage() {
 for script in $(find /tmp/addon.d/ -name '*.sh' |sort -n); do
@@ -29,12 +48,14 @@ done
 case "$1" in
   backup)
     mkdir -p $C
+    xposed_backup
     preserve_addon_d
     run_stage pre-backup
     run_stage backup
     run_stage post-backup
   ;;
   restore)
+    xposed_restore
     run_stage pre-restore
     run_stage restore
     run_stage post-restore
